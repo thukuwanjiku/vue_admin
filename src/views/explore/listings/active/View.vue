9e7c997756b9435ebe1f3991130f81674e49c0e2
thukuwanjiku <thukuwanjikum@gmail.com>
2024-09-08 00:47:57 +0300

    - getting started with view listing

--- src/views/explore/listings/active/View.vue
+++ src/views/explore/listings/active/View.vue
@@ -1,11 +1,52 @@
 <script setup>
 
+import {useStore} from "vuex";
+import {useRouter} from "vue-router";
+import CloseButton from "@/components/CloseButton.vue";
+import {onMounted, ref} from "vue";
+
+/* ----------------------------------
+* Variables & properties
+* ----------------------------------
+* */
+const store = useStore();
+const router = useRouter();
+
+const listing = ref(null);
+const listingMedia = ref([]);
+
+/* ----------------------------------
+* Lifecycle Hooks
+* ----------------------------------
+* */
+onMounted(()=>{
+    if(store.state.exploreHub.viewedListing == null){
+        return router.back();
+    }
+
+    //copy listing to local variable
+    listing.value = JSON.parse(JSON.stringify(store.state.exploreHub.viewedListing));
+
+    //copy listing media to variable for managing them
+    listingMedia.value = JSON.parse(JSON.stringify(listing.value.media));
+});
+
 </script>
 
 <template>
 
     <div class="row">
-        <h5>View listing</h5>
+        <div class="col-sm-12 mb-3 d-inline-flex justify-content-end">
+            <close-button></close-button>
+        </div>
+
+        <div class="col-md-12">
+            <el-carousel :interval="4000" indicator-position="outside">
+                <el-carousel-item v-for="(file, index) in listingMedia" :key="'listing-media-'+index">
+                    <img style="max-height: 200px;" :src="file.path">
+                </el-carousel-item>
+            </el-carousel>
+        </div>
     </div>
 
 </template>
