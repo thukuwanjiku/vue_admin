679e6df8b5826fa3ac03ce101cf51a15c955f2c4
thukuwanjiku <thukuwanjikum@gmail.com>
2024-09-27 13:09:28 +0300

    - npm run build

--- src/views/explore_hub/categories/Main.vue
+++ src/views/explore_hub/categories/Main.vue
@@ -46,11 +46,11 @@ onMounted(()=>{
                             data-bs-toggle="tab" data-bs-target="#explore_categories_active"
                             type="button" role="tab" aria-controls="home" aria-selected="false" tabindex="-1">Categories</button>
                 </li>
-                <li class="nav-item" role="presentation" v-if="hasPermissionsWhichContain(['explore_hub.archived_categories'])">
+<!--                <li class="nav-item" role="presentation" v-if="hasPermissionsWhichContain(['explore_hub.archived_categories'])">
                     <button class="nav-link"
                             data-bs-toggle="tab" data-bs-target="#explore_categories_deleted"
                             type="button" role="tab" aria-controls="contact" aria-selected="true">Archived</button>
-                </li>
+                </li>-->
             </ul>
             <div class="tab-content pt-2" id="myTabContent">
                 <div class="tab-pane fade show active p-1" id="explore_categories_active"
